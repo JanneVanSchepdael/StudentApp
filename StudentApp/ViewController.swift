78ccff25a5283ceaaff15e35dcb52e542d9a2109
Janne <janne.vanschepdael@student.hogent.be>
2021-07-06 08:37:43 -0700

    Initial commit

---
+++ StudentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentApp
+//
+//  Created by Atlas on 18/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
