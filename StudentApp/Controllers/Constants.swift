36343348bd4c217eee1f0e209d1eb4cbef804190
Janne <janne.vanschepdael@student.hogent.be>
2021-07-08 05:01:57 -0700

    Added viewtable with Delegate & data

---
+++ StudentApp/Controllers/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  StudentApp
+//
+//  Created by Atlas on 08/07/2021.
+//
+
+import Foundation
+
+struct Constants {
+    static var POSTCELL_ID = "PostCell"
+}
--- StudentApp/Controllers/ViewController.swift
+++ StudentApp/Controllers/ViewController.swift
@@ -7,19 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     var eventRepository: EventRepository = EventRepository();
+    var events = [Event]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set ViewController as datasource & delegate
+        tableView.dataSource = self
+        tableView.delegate = self
         
         eventRepository.getEventsFromJSON()
     }
-
-    //Test commit from VS
- 
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.POSTCELL_ID, for: <#T##IndexPath#>)
+        
+        //TO DO: configure cell with data
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
 
 }
 
